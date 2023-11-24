563ffe3b0dab231d20f0819db2b94fd3e3c65b40
Aditya Arakharao <ararakharao_b21@it.vjti.ac.in>
2023-11-24 18:50:05 +0530

    Added all sound blocks

---
+++ Desktop/Blokly Intern/LogicBlocks_TeamC/src/components/BlocklyComponent.jsx
@@ -0,0 +1,56 @@
+// Updated BlocklyComponent.jsx
+import React, { useEffect, useRef } from "react";
+import Blockly from "blockly";
+import { Logic } from "./BlockCategories/Logic";
+import { Loops } from "./BlockCategories/Loops";
+import { Math } from "./BlockCategories/Math";
+import { Text } from "./BlockCategories/Text";
+import initializeBlockly from "./InitializeBlockly"; // import the function
+import { Sounds } from "./BlockCategories/Sounds";
+
+const BlocklyComponent = () => {
+  const blocklyRef = useRef(null);
+
+  useEffect(() => {
+    if (blocklyRef.current === null) {
+      // Initialize Blockly with English
+      Blockly.setLocale("en");
+      // Construct the complete toolbox XML
+      const toolboxXml = `
+        <xml id="toolbox" style="display: none">
+          ${Logic}
+          ${Loops}
+          ${Math}
+          ${Text}
+          ${Sounds}
+        </xml>
+      `;
+      initializeBlockly(toolboxXml); // Initialize Blockly using the separate function
+      blocklyRef.current = true;
+    }
+  }, []);
+
+  return (
+    <div style={{ width: "100%", height: "480px" }}>
+      <h1
+        style={{
+          display: "inline-block",
+          fontSize: "14px",
+          marginRight: "500px",
+        }}
+      >
+        Blockly Toolbox
+      </h1>
+      <h1 style={{ display: "inline-block", fontSize: "14px" }}>
+        Blockly Workspace
+      </h1>
+      <div
+        className="highlighted"
+        id="blocklyDiv"
+        style={{ height: "100%", width: "100%", position: "relative" }}
+      ></div>
+    </div>
+  );
+};
+
+export default BlocklyComponent;
